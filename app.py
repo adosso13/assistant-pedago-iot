14fd68977e2f354f380e7bbb3d3b4d57ae9dba7e
Adosso13 <ahmed.dosso13@gmail.com>
2026-05-11 16:23:21 +0200

    Ahmed, 11/05/2026

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
+#page d'accuil
 def home():
     return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    question = request.form['question']
+    return render_template('response.html', question=question)
+
+# page hypotetique pour recuil de une reponse 
+@app.route('/reponse')
+def reponse():
+    return render_template('response.html')
+
+#page qcm
 @app.route('/qcm')
 def qcm():
     return render_template('qcm.html')
 
+#page challenge
 @app.route('/challenge')
 def challenge():
     return render_template('challenge.html')
-
-"""
-@app.route('/challenge/theme1')
-def theme1():
-    return render_template('theme1.html')
-"""
 
 if __name__ == '__main__':
     app.run(debug=True)
